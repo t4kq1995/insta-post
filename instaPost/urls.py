fac8322ceec158572324c715d6ce1f64a8f9fc4c
t4kq1995 <vlad13zp@mail.ru>
2017-04-03 15:04:36 +0300

    Test example creation

--- instaPost/urls.py
+++ instaPost/urls.py
@@ -6,6 +6,10 @@ from instaPost import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
+    # Instagram upload image
+    url(r'^upload_image/$', views.upload_photo),
+    # Splinter parse
+    url(r'^get_info/$', views.splinter_parse),
     # Auth
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- instaPost/views.py
+++ instaPost/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+import os.path
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import permissions
+from splinter import Browser
 
-# Create your views here.
+
+""" Global variables """
+BASE = os.path.dirname(os.path.abspath(__file__))
+LOGIN = "t4kq"
+PASSWORD = "pro100vladpro100vlad"
+
+
+@api_view(['GET'])
+@permission_classes((permissions.IsAuthenticated,))
+def upload_photo(request):
+    if request.method == 'GET':
+        from InstagramAPI import InstagramAPI
+        inst_api = InstagramAPI(LOGIN, PASSWORD)
+        inst_api.login()
+        inst_api.uploadPhoto(os.path.join(BASE, "../images/image.jpg"), 'Hello')
+        return JsonResponse({'status': True})
+
+
+@api_view(['GET'])
+@permission_classes((permissions.IsAuthenticated,))
+def splinter_parse(request):
+    if request.method == 'GET':
+        with Browser('chrome') as browser:
+            # Visit URL
+            url = "http://www.google.com"
+            browser.visit(url)
+            browser.fill('q', 'splinter - python acceptance testing for web applications')
+            # Find and click the 'search' button
+            button = browser.find_by_name('btnG')
+            # Interact with elements
+            button.click()
+            if browser.is_text_present('splinter.readthedocs.io'):
+                print("Yes, the official website was found!")
+            else:
+                print("No, it wasn't found... We need to improve our SEO techniques")
+        return JsonResponse({'status': True})
